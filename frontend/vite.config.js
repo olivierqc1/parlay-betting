5f45a5caab063974f6076210d866b0cb0a635ccd
olivierqc1 <olivierturgeon@hotmail.fr>
2026-03-01 16:40:44 +0100

    Actualizar vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,1 +1,9 @@
-aha
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// "base" doit correspondre exactement au nom de ton repo GitHub
+// ex: repo "parlay-edge" → base: "/parlay-edge/"
+export default defineConfig({
+  plugins: [react()],
+  base: "/parlay-edge/",
+});
